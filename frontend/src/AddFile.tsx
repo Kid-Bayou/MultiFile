a13fb523054426380e3a622e1afef918234d5db3
Kid-Bayou <88617617+Kid-Bayou@users.noreply.github.com>
2024-05-02 21:17:22 +0300

    Edited the styling

--- frontend/src/AddFile.tsx
+++ frontend/src/AddFile.tsx
@@ -1,21 +1,30 @@
 import { useNavigate } from "react-router-dom";
 
 const AddFile: React.FC = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-    const handleAdd = () => {
-        navigate("/successpage");
-    }
-  
-    return (
-      <>
-          <h2>Add File</h2>
-          <button onClick={handleAdd}>Add</button>
-      </>
-    );
+  const handleAdd = () => {
+    navigate("/successpage");
   };
-  
-  
-  export default AddFile;
-  
+
+  return (
+    <>
+      <div className="add-file-container">
+        <h2>Add File</h2>
+        <form onSubmit={handleAdd} className="add-file-form">
+          <div className="add-file-textarea">
+            <label>Description:</label>
+            <textarea />
+          </div>
+          <div className="add-file-textarea">
+          <label>Select File:</label>
+          <input type="file" />
+          </div>
+          <button>Add</button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default AddFile;
--- frontend/src/UpdateFile.tsx
+++ frontend/src/UpdateFile.tsx
@@ -1,25 +1,31 @@
 import { useNavigate } from "react-router-dom";
 
 const UpdateFile: React.FC = () => {
-
   const navigate = useNavigate();
-  
-  const handleUpdate = () => {
-    navigate("/successpage")
-  }
 
-    return (
-      <>
-          <h2>Update File</h2>
-          <form onSubmit={handleUpdate}>
+  const handleUpdate = () => {
+    navigate("/successpage");
+  };
+
+  return (
+    <>
+      <div className="update-file-container">
+        <h2>Update File</h2>
+        <form onSubmit={handleUpdate} className="update-file-form">
+          <div className="update-file-textarea">
             <label>Description:</label>
             <textarea />
-            <button>Update</button>
-          </form>
-      </>
-    );
-  };
-  
-  
-  export default UpdateFile;
-  
+          </div>
+          <div className="update-file-textarea">
+            <label>Select File:</label>
+            <input type="file" />
+          </div>
+
+          <button>Update</button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default UpdateFile;
